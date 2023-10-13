1d98fba3af4d6ac0154ec7d44001aca8d11f7479
isaías <isaiasleite230@gmail.com>
2023-10-13 14:23:38 -0300

    Construtores

---
+++ Classes/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Classes {
+    class Calculator {
+
+        public static double PI = 3.14;
+
+        public static double Circunferencia(double r) {
+            return 2.0 * PI * r;
+        }
+
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * PI * Math.Pow(r, 3);
+        }
+    }
+}
--- Classes/Product.cs
+++ Classes/Product.cs
@@ -6,24 +6,30 @@ using System.Threading.Tasks;
 
 namespace PrimeiroProjeto.Classes {
     internal class Product {
-        public string nome;
-        public double preco;
-        public int quantidade;
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Product(string nome, double preco, int quantidade) {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
 
         public double ValorTotalEmEstoque() {
-            return quantidade * preco;
+            return Quantidade * Preco;
         }
 
         public void AdicionarProduto(int x) {
-            this.quantidade += x;
+            this.Quantidade += x;
         }
 
         public void RemoverProduto(int x) {
-            this.quantidade -= x;
+            this.Quantidade -= x;
         }
 
         public override string ToString() {
-            return nome + ", R$" + preco.ToString("F2") + ", " + quantidade + " unidades, Total: $" + ValorTotalEmEstoque();
+            return Nome + ", R$" + Preco.ToString("F2") + ", " + Quantidade + " unidades, Total: $" + ValorTotalEmEstoque();
         }
     }
 }
