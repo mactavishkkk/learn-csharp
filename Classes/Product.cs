0614b98b7a5fa6ab7df4c833453ba4d1fd0edd12
isaías <isaiasleite230@gmail.com>
2023-10-14 14:59:29 -0300

    Encapsulamento

--- Classes/Product.cs
+++ Classes/Product.cs
@@ -5,38 +5,62 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace PrimeiroProjeto.Classes {
-    internal class Product {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+    class Product {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
 
         public Product(string nome, double preco, int quantidade) {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
         }
 
         public Product(string nome, double preco) {
-            Nome = nome;
-            Preco = preco;
+            _nome = nome;
+            _preco = preco;
         }
 
         public Product() { }
 
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+            _nome = nome;
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public void SetPreco(double preco) {
+            _preco = preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade) {
+            _quantidade = quantidade;
+        }
+
         public double ValorTotalEmEstoque() {
-            return Quantidade * Preco;
+            return _quantidade * _preco;
         }
 
         public void AdicionarProduto(int x) {
-            this.Quantidade += x;
+            this._quantidade += x;
         }
 
         public void RemoverProduto(int x) {
-            this.Quantidade -= x;
+            this._quantidade -= x;
         }
 
         public override string ToString() {
-            return Nome + ", R$" + Preco.ToString("F2") + ", " + Quantidade + " unidades, Total: $" + ValorTotalEmEstoque();
+            return _nome + ", R$" + _preco.ToString("F2") + ", " + _quantidade + " unidades, Total: $" + ValorTotalEmEstoque();
         }
     }
 }
