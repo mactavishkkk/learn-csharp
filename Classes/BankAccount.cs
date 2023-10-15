56f0280b9f107644b69951e494b738c742663fe0
isaías <isaiasleite230@gmail.com>
2023-10-15 16:24:09 -0300

    Exercício de construtores

---
+++ Classes/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Classes {
+    class BankAccount {
+        private int _numeroConta;
+        private string _nomeTitular;
+        private double _saldo;
+
+        public BankAccount(int numeroConta, string nomeTitular, double saldo) {
+            _numeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            _saldo = saldo;
+        }
+
+        public BankAccount(int numeroConta, string nomeTitular) {
+            _numeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            _saldo = 0;
+        }
+
+        public string NomeTitular {
+            get { return _nomeTitular; }
+            set { _nomeTitular = value; }
+        }
+
+        public void Deposito(double valor) {
+            _saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            _saldo -= valor;
+            _saldo -= 5.00;
+        }
+
+        public override string ToString() {
+            return "\nDados da conta: \nConta - " + _numeroConta +
+                ", Titular: " + _nomeTitular + ", Saldo: $ " + _saldo.ToString("F2") + "\n";
+        }
+    }
+
+}
---
+++ Exercises/Constructors.cs
@@ -0,0 +1,38 @@
+﻿using PrimeiroProjeto.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto.Exercises {
+    class Constructors {
+        static void Main(string[] args) {
+            int numeroConta;
+            string nomeTitular;
+            double valor;
+            string depositoInicial;
+
+            Console.Write("Entre com o número da conta: "); numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o nome do titular da conta: "); nomeTitular = Console.ReadLine();
+            Console.Write("Deseja fazer um depósito inicial? (S/N) "); depositoInicial = Console.ReadLine();
+            BankAccount account = new BankAccount(numeroConta, nomeTitular);
+
+            if (depositoInicial == "S") {
+                Console.Write("Entre com o valor do depósito: "); valor = double.Parse(Console.ReadLine());
+                account.Deposito(valor);
+                Console.WriteLine(account);
+            } else {
+                Console.WriteLine(account);
+            }
+
+            Console.Write("Entre com um valor para depósito: "); valor = double.Parse(Console.ReadLine());
+            account.Deposito(valor);
+            Console.WriteLine(account);
+
+            Console.Write("Entre com um valor para saque: "); valor = double.Parse(Console.ReadLine());
+            account.Saque(valor);
+            Console.WriteLine(account);
+        }
+    }
+}
