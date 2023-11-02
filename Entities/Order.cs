e438d1c5fab74cf6ca03a8fe1691daf0cbae74de
isaías <isaiasleite230@gmail.com>
2023-11-02 00:30:13 -0300

    Início em Enums

---
+++ Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using PrimeiroProjeto.Entities.Enums;
+using System;
+
+namespace PrimeiroProjeto.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + ", Data/hora: " + Moment + ", Status: " + Status;
+        }
+    }
+}
---
+++ Enums.cs
@@ -0,0 +1,23 @@
+﻿using PrimeiroProjeto.Entities;
+using PrimeiroProjeto.Entities.Enums;
+using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Enums
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order { 
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); // cw(txt); -> PendingPayment(0)
+            OrderStatus status = Enum.Parse<OrderStatus>("Delivered"); // cw(status); -> Delivered(3)
+        }
+    }
+}
