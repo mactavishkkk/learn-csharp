e9f3c3e8c0ddc8f9d1d79d4e172f04f31cc40613
isaías <isaiasleite230@gmail.com>
2023-11-09 01:49:19 -0300

    UP/DOWN casting

---
+++ Classes/Account/SavingsAccount.cs
@@ -0,0 +1,22 @@
+﻿using PrimeiroProjeto.Classes.Order;
+
+namespace PrimeiroProjeto.Classes.Account
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
---
+++ UpDownCasting.cs
@@ -0,0 +1,54 @@
+﻿using PrimeiroProjeto.Classes.Account;
+
+namespace PrimeiroProjeto
+{
+    internal class UpDownCasting
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+            BusinessAccount businessAccount = new BusinessAccount();
+
+            // UP
+
+            Account account1 = businessAccount;
+            Account account2 = new BusinessAccount(1003, "Gabriela", 100, 200);
+            /* 
+             * account2.Loan(200); Para o compilador, esta variável (account2) é um Account, logo, apesar de ser instânciada como "Business",
+            ela só possui as operações de "Account" 
+            */
+            account2.Withdraw(10.5);
+            Account account3 = new SavingsAccount(1003, "Gabriela", 100, 1.0);
+
+            // DOWN
+
+            /* 
+             * BusinessAccount businessAccount2 = account2; | Para o compilador, ele só toma como parâmetro 
+             * o tipo da variável, que é "Account" 
+             */
+
+            // Logo, é necessário este casting para funcionar
+
+            BusinessAccount businessAccount2 = (BusinessAccount)account2;
+            businessAccount2.Loan(100.50);
+
+            // Também é necessário o down casting, mas apesar disto, o erro ocorre em tempo de execução
+            BusinessAccount businessAccount3 = (BusinessAccount)account3;
+
+            // Tratando excessões acima
+            if (account3 is BusinessAccount)
+            {
+                BusinessAccount businessAccount1 = (BusinessAccount)account3;
+                businessAccount1.Loan(157.6);
+                Console.WriteLine("Loan!");
+            }
+
+            if (account3 is SavingsAccount)
+            {
+                SavingsAccount savingsAccount = account3 as SavingsAccount;
+                savingsAccount.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+        }
+    }
+}
