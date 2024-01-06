e4dcde0778547e45a03e2607d1c85e146270414c
isaías <isaiasleite230@gmail.com>
2024-01-06 13:37:11 -0300

    StreamReader com using

--- Archives.cs
+++ Archives.cs
@@ -8,26 +8,22 @@ namespace PrimeiroProjeto
         static void Main(string[] args)
         {
             string path = @"C:\Users\mac\Documents\projetos\PrimeiroProjeto\PrimeiroProjeto\file.txt";
-            FileStream FS = null;
-            StreamReader SR = null;
+
             try
             {
-                FS = new FileStream(path, FileMode.Open);
-                SR = new StreamReader(FS);
-                while (!SR.EndOfStream)
+                using (StreamReader SR = File.OpenText(path))
                 {
-                    string line = SR.ReadLine();
-                    Console.WriteLine(line);
+                    while (!SR.EndOfStream)
+                    {
+                        string line = SR.ReadLine();
+                        Console.WriteLine(line);
+                    }
                 }
 
             } catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
-            } finally
-            {
-                if (SR != null) SR.Close();
-                if (FS != null) FS.Close();
             }
         }
     }
