0bbf37b3738deac85ddd278ec5cf8b2189ae588d
isaías <isaiasleite230@gmail.com>
2024-01-27 14:19:20 -0300

    HashSet/SortedSet exercise

---
+++ Classes/LogRegister.cs
@@ -0,0 +1,24 @@
+﻿namespace PrimeiroProjeto.Classes
+{
+    internal class LogRegister
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is LogRegister))
+            {
+                return false;
+            }
+
+            LogRegister other = obj as LogRegister;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ Exercises/Set.cs
@@ -0,0 +1,35 @@
+﻿using PrimeiroProjeto.Classes;
+
+namespace PrimeiroProjeto.Exercises
+{
+    internal class Set
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRegister> set = new HashSet<LogRegister>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+
+                        set.Add(new LogRegister { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine("File path: " + path);
+                    Console.WriteLine("Total users: " + set.Count());
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
